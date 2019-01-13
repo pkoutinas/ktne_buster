92ddb541ceaade74cc1d55866f95e5d3827b6129
pkoutinas <43806676+pkoutinas@users.noreply.github.com>
2019-01-13 13:11:41 +0100

    Added image assets and logo on home screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,50 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: new ThemeData(
-        primaryColor: Colors.white,
+        primaryColor: Colors.black,
       ),
+      home: new MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body:
+      new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  new Image.asset(
+                    'images/logo.png',
+                    fit:BoxFit.fill,
+                  )
+                ]
 
+            )
+          ]
+
+      ),
+
+    );
+  }
+}
