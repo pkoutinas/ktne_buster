8e3c8445188b2fd60dbe8bd135f9f1a63c1669d1
pkoutinas <43806676+pkoutinas@users.noreply.github.com>
2019-01-28 23:28:25 +0100

    2019-01-28:  - Basic layout for MemoryScreen done (cosmtics left). No logic implemented.

--- lib/MemoryScreen.dart
+++ lib/MemoryScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'Configurator.dart';
-import 'helper.dart';
 
 class MemoryScreen extends StatefulWidget {
   final Configurator config;
@@ -12,9 +11,43 @@ class MemoryScreen extends StatefulWidget {
 }
 
 class _MemoryScreenState extends State<MemoryScreen> {
+  Map<int,List<int>> memoryMap;
+  int _currentStep = 0;
+
+  List<Step> _mySteps(){
+    List<Step> _steps = [
+      Step(
+        title: Text(""),
+        content: Container(height: 220,child:Text("")),
+        isActive: _currentStep>=0,
+      ),
+      Step(
+        title: Text(""),
+        content: Container(height: 220,child:Text("")),
+        isActive: _currentStep>=1,
+      ),
+      Step(
+        title: Text(""),
+        content: Container(height: 220,child:Text("")),
+        isActive: _currentStep>=2,
+      ),
+      Step(
+        title: Text(""),
+        content: Container(height: 220,child:Text("")),
+        isActive: _currentStep>=3,
+      ),
+      Step(
+        title: Text(""),
+        content: Container(height: 220,child:Text("")),
+        isActive: _currentStep>=4,
+      )
+    ];
+    return _steps;
+  }
 
   void _resetProgress(){
     setState(() {
+      _currentStep=0;
     });
   }
 
@@ -39,11 +72,112 @@ class _MemoryScreenState extends State<MemoryScreen> {
           backgroundColor: Colors.red,
         ),
         body:
-        Column(
-            children: <Widget>[
-
-            ])
-    );
+            Container(
+              padding: EdgeInsets.all(40),
+              child:
+            Stack(children: <Widget>[
+              Container(
+                decoration: BoxDecoration(border: Border.all(color: Colors.white)),
+                width: 300,
+                height: 380,
+                child:
+                    Container(
+                        decoration: BoxDecoration(color: Colors.white),
+                        child:
+                    Stepper(
+                      type: StepperType.horizontal,
+                      currentStep: this._currentStep,
+                      controlsBuilder:
+                          (BuildContext context, {
+                            VoidCallback onStepContinue,
+                            VoidCallback onStepCancel}) {
+                        return Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              (this._currentStep!=this._mySteps().length-1)
+                                  ?FlatButton(
+                                      onPressed: onStepContinue,
+                                      color: Colors.red,
+                                      child: const Text('Next'))
+                                  : FlatButton(
+                                  onPressed: _resetProgress,
+                                  color: Colors.white,
+                                  child: const Text("Done",
+                                      style: TextStyle(color: Colors.black)))
+                            ],
+                          );
+                        },
+                      steps: _mySteps(),
+                      onStepTapped: (step){},
+                      onStepCancel: (){},
+                      onStepContinue: (){
+                        setState(() {
+                          if(this._currentStep < this._mySteps().length-1){
+                            this._currentStep += 1;
+                          }
+                        });},
+                  ))
+              ),
+              Positioned(
+                left: 15,
+                top: 100,
+                child: Container(
+                  child:
+                  Column(
+                      children: <Widget>[
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Container(
+                                  margin: EdgeInsets.only(bottom:2, left:2, right:2),
+                                  child:
+                                  Container(
+                                    width: 246,
+                                    height: 130,
+                                    alignment: Alignment(0, 0),
+                                    decoration: BoxDecoration(color: Colors.black),
+                                    child: Text("HI"),
+                                  )),
+                            ]),
+                        Row( //TODO: why is spacing between tiles lost since it moved to stack?
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Container(
+                                width: 60,
+                                alignment: Alignment(0, 0),
+                                decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                                padding: EdgeInsets.only(top:20,left:2,right: 2, bottom: 30),
+                                child: Text("HI", style: TextStyle(color: Colors.black),),
+                              ),
+                              Container(
+                                width: 60,
+                                alignment: Alignment(0, 0),
+                                decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                                padding: EdgeInsets.only(top:20,left:2,right: 2, bottom: 30),
+                                child: Text("HI", style: TextStyle(color: Colors.black),),
+                              ),
+                              Container(
+                                width: 60,
+                                alignment: Alignment(0, 0),
+                                decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                                padding: EdgeInsets.only(top:20,left:2,right: 2, bottom: 30),
+                                child: Text("HI", style: TextStyle(color: Colors.black),),
+                              ),
+                              Container(
+                                width: 60,
+                                alignment: Alignment(0, 0),
+                                decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                                padding: EdgeInsets.only(top:20,left:2,right: 2, bottom: 30),
+                                child: Text("HI", style: TextStyle(color: Colors.black),),
+                              ),
+                            ]),
+                      ]),
+                ),
+              ),
+        ])
+    ));
   }
 }
 
